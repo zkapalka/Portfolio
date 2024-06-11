e78e449e54c16b84505082f88952b519acc07b93
zkapalka <zkapalka@indeed.com>
2024-06-11 14:45:24 -0500

    updated projects

--- personal-portfolio/src/components/Main/Project/Project.jsx
+++ personal-portfolio/src/components/Main/Project/Project.jsx
@@ -7,40 +7,34 @@ const Project = () => {
       image: "src/assets/portfolio.jpg",
       alt: "Picture of the word portfolio",
       projectName: "Personal Portfolio",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      description: "A personal portfolio project using JavaScript",
     },
     { 
       link: "https://git.generalassemb.ly/zkapalka/Week_4",
       image: "src/assets/shoe.jpeg",
       alt: "Collection of different shoes arranged in the color of a rainbow",
       projectName: "DEI JavaScript Project",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      description: "A prototype of a job board aimed at Deaf people using JavaScript",
     },
     {
       link: "https://git.generalassemb.ly/zkapalka/week_6/tree/main/day_3",
       image: "src/assets/dictionary-words-grammar-education-8b6784-1024 (1).jpg",
       alt: "Pic of the definition of dictionary",
       projectName: "Dictionary Java App",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      description: "A basic dictionary app using Java",
     },
     { 
       link: "https://git.generalassemb.ly/zkapalka/week_12_SpringProject",
       image: "src/assets/Back flex.jpg",
       alt: "Drawing of man flexing back",
       projectName: "Backend Spring Project",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      description: "A project mimicking basic CRUD services for a job board using Spring and PostreSQL",
     },
     {
-      image: "https://via.placeholder.com/300x200",
-      alt: "Project 5",
-      projectName: "Placeholder",
-      description: "Placeholder for upcoming React DEI project",
-    },
-    {
-      image: "https://via.placeholder.com/300x200",
+      image: "src/assets/Capstone.jpg",
       alt: "Project 6",
-      projectName: "Placeholder",
-      description: "placeholder for upcoming capstone project",
+      projectName: "Capstone Project",
+      description: "My capstone using React, Spring, and PostreSQL",
     }
   ];
   return (
