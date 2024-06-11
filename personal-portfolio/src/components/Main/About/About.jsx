ed62433a22087dfc612c2e36ce6d320983a5782c
zkapalka <zkapalka@indeed.com>
2024-06-11 15:09:44 -0500

    updated project links

--- personal-portfolio/src/components/Main/About/About.jsx
+++ personal-portfolio/src/components/Main/About/About.jsx
@@ -10,7 +10,13 @@ const About = () => {
             and other SWE disciplines. I enjoy developing applications at enterprise
             scale to better serve various companies and clients.
           </p>
-          <br></br>
+          <br />
+          <p>I started on this software engineer journey because I wanted to be able to peek into the world that my dad lived in for 
+            over 30 years, and I wanted to be able to find something that I would be able to keep doing for the rest of my life, and 
+            thanks to the multiple disciplines in the software engineering world, I've found it. While I'm on this adventure, I want to
+            be able to eventually specialize in cloud, SRE or machine learning, and see just how far I'm capable of going.
+            </p>
+          <br />
           <p>
             With my background and passion for building robust systems, I focus on 
             creating clean, efficient codebases that scales and meet standards across 
--- personal-portfolio/src/components/Main/Project/Project.jsx
+++ personal-portfolio/src/components/Main/Project/Project.jsx
@@ -3,34 +3,35 @@ import ProjectCard from "../../ProjectCard/ProjectCard";
 const Project = () => {
   const projectCardData = [
     {
-      link: "https://git.generalassemb.ly/zkapalka/personal_portfolio",
+      link: "https://github.com/zkapalka/GA_Stuff/tree/main/personal_portfolio-master",
       image: "src/assets/portfolio.jpg",
       alt: "Picture of the word portfolio",
       projectName: "Personal Portfolio",
       description: "A personal portfolio project using JavaScript",
     },
     { 
-      link: "https://git.generalassemb.ly/zkapalka/Week_4",
+      link: "https://github.com/zkapalka/GA_Stuff/tree/main/Week_4-master",
       image: "src/assets/shoe.jpeg",
       alt: "Collection of different shoes arranged in the color of a rainbow",
       projectName: "DEI JavaScript Project",
       description: "A prototype of a job board aimed at Deaf people using JavaScript",
     },
     {
-      link: "https://git.generalassemb.ly/zkapalka/week_6/tree/main/day_3",
+      link: "https://github.com/zkapalka/GA_Stuff/tree/main/week_6-main/day_3/java-project-dictionary-application-master",
       image: "src/assets/dictionary-words-grammar-education-8b6784-1024 (1).jpg",
       alt: "Pic of the definition of dictionary",
       projectName: "Dictionary Java App",
       description: "A basic dictionary app using Java",
     },
     { 
-      link: "https://git.generalassemb.ly/zkapalka/week_12_SpringProject",
+      link: "https://github.com/zkapalka/GA_Stuff/tree/main/week_12_SpringProject-develop",
       image: "src/assets/Back flex.jpg",
       alt: "Drawing of man flexing back",
       projectName: "Backend Spring Project",
       description: "A project mimicking basic CRUD services for a job board using Spring and PostreSQL",
     },
     {
+      link: "https://github.com/zkapalka/GA_Stuff/tree/main/capstone",
       image: "src/assets/Capstone.jpg",
       alt: "Project 6",
       projectName: "Capstone Project",
