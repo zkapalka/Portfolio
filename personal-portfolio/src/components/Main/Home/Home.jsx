95b07fb94c8416b2041102ce52b1ed13b4c19691
zkapalka <zkapalka@indeed.com>
2024-06-12 16:15:26 -0500

    updated button home and link to github

--- personal-portfolio/src/components/Main/Home/Home.jsx
+++ personal-portfolio/src/components/Main/Home/Home.jsx
@@ -1,11 +1,16 @@
 const Hero = () => {
     return (
       <section className="hero">
-        <div className="hero-content">
-          <h1>Welcome to My Portfolio</h1>
-          <p>Explore my work and skills.</p>
-          <button>Learn More</button>
-        </div>
+      
+          <h1> Kapalka Time</h1>
+          <p>Welcome! Please feel free to check out my projects and my skills below. 
+            <br />Additionally, if you're curious about the code that goes
+            into this portfolio, please click on the button below to check out the Github repo for this.
+          </p>
+          <a href="https://github.com/zkapalka/Portfolio" target="_blank">
+            <button>Personal Portfolio Github Repo</button>
+          </a>
+        
       </section>
     );
   };
