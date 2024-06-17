9859233b47320fed193309c279024391b6fde522
zkapalka <zkapalka@indeed.com>
2024-06-17 11:23:06 -0500

    completed feedback

--- personal-portfolio/src/components/Main/MentorProfile/MentorProfile.jsx
+++ personal-portfolio/src/components/Main/MentorProfile/MentorProfile.jsx
@@ -38,7 +38,47 @@ const MentorBio = styled.p`
   max-width: 600px;
 `;
 
+
+const FeedbackContainer = styled.div`
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  padding: 1rem;
+  width: 100%;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  margin-top: 2rem;
+`;
+
+const FeedbackHeader = styled.h3`
+  font-size: 1.2rem;
+  color: #333;
+  margin-bottom: 1rem;
+`;
+
+const FeedbackList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const FeedbackItem = styled.li`
+  font-size: 1rem;
+  color: #555;
+  padding: 0.5rem 0;
+  border-bottom: 1px solid #eee;
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
 const MentorProfile = () => {
+  const feedbackData = [
+    "We reviewed his project this week and he was able to clearly define why he made various choices as well as ways in which he would do things differently / improve it further which all show a great understanding of what he did.  He's more comfortable taking advantage of the resources available to him (such as ChatGPT) and understands the limitations of them.  He asks great questions and is clearly already growing as a developer.  Making great progress.",
+    "Zach continues to ask great questions and shows a good understanding of the concepts covered this week.  He says Java itself isn't difficult to transition to although admits some of the topics about inheritance / polymorphism / abstraction / etc. is hard to wrap his mind around.  We've discussed it a fair bit this week and I think it'll get easier for him to see as he gets some more concrete examples.  He has also expressed a desire to see how development works on a team. ",
+    "We did a code review on Zach and Emily's projects today which went well and they found value in.  ",
+    "Zach continues to build a strong base of knowledge he can draw on for new concepts which seems to really be helping him pick up new materials.  It doesn't sound as stressful to learn new things as it used to be - as an example, he immediately picked up on why React can be so powerful and seemed eager to dig into it more this week."
+  ];
+  
   return (
     <ProfileContainer id='mentor'>
       <h2 >Mentor Profile</h2>
@@ -50,6 +90,14 @@ const MentorProfile = () => {
       (pre-teens, lots of pets, active in our church, DIY #ftw).  I've done frontend, backend, and devops automation over my career.  
       Quote - "If you don't hate some of your code from 1 year ago, you aren't growing as a developer."
       </MentorBio>
+      <FeedbackContainer>
+        <FeedbackHeader>Feedback for Zach the Mentee</FeedbackHeader>
+        <FeedbackList>
+          {feedbackData.map((feedback, index) => (
+            <FeedbackItem key={index}>{feedback}</FeedbackItem>
+          ))}
+        </FeedbackList>
+      </FeedbackContainer>
     </ProfileContainer>
   );
 };
